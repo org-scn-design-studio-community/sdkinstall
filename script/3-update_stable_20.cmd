88a3d29b4c6bbdd1aad790d81adfd99fbbdee924
Karol Kalisz <karol@kalisz.org>
2015-07-16 10:10:20 +0200

    - updating help

--- script/3-update_stable_20.cmd
+++ script/3-update_stable_20.cmd
@@ -1,5 +1,10 @@
 set current=%cd%
 
+cd ..\releases\preview\
+ren preview.zip org.scn.community.sdk.package_preview.zip
+
+cd %current%
+
 cd ..\releases\stable\features\
 del *.* /q
 
@@ -17,4 +22,6 @@ copy ..\releases\preview\features\*.* ..\releases\stable\features\
 copy ..\releases\preview\plugins\*.* ..\releases\stable\plugins\
 copy ..\releases\preview\*.jar ..\releases\stable\
 copy ..\releases\preview\*.xml ..\releases\stable\
+
+ren ..\releases\preview\preview.zip ..\releases\preview\org.scn.community.sdk.package_preview.zip
 copy ..\releases\preview\org.scn.community.sdk.package_preview.zip ..\releases\stable\org.scn.community.sdk.package_stable.zip
